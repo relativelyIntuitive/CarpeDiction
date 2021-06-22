06716e925ca6af42c19009b3b7d03266cf547985
PhlankStake <z.bielicki88@gmail.com>
2021-06-21 20:37:20 -0400

    ~shortened wotd retrieval to 6 hrs for now...

--- server/server.js
+++ server/server.js
@@ -48,7 +48,6 @@ setInterval(
             .catch(err => console.log(err));
     },
     // repeat every 24 hours (expressed in ms)
-    86400000
-    // 5 second repeat to uncomment for initial archive population (expressed in ms)
-    // 500000
+    // 86400000
+    21600000
 )
